855863d6eebcde783bd8789c46da7ced5d63e816
Dustin Holman <dustinwholman@gmail.com>
2018-11-05 19:58:22 -0800

    breaking out sync/decision logic

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,18 @@ use std::time::SystemTime;
 
 const SLEEP_DURATION: Duration = Duration::from_millis(2000);
 
-struct SynkFile<'a> {
+struct Copier<'a> {
+    from: &'a SynkFile<'a>,
+    to: &'a SynkFile<'a>,
+}
+
+#[derive(Clone, Copy)]
+pub struct SynkFile<'a> {
     p: &'a Path,
 }
 
 impl<'a> SynkFile<'a> {
-    fn new(fname: &'a str) -> Self {
+    pub fn new(fname: &'a str) -> Self {
         let pt = Path::new(fname);
         SynkFile { p: pt }
     }
@@ -22,50 +28,54 @@ impl<'a> SynkFile<'a> {
         fs::metadata(&self.p)
     }
 
-    fn size(&self) -> u64 {
+    pub fn len(&self) -> u64 {
         self.metadata().unwrap().len()
     }
 
-    fn mod_time(&self) -> u64 {
+    pub fn mod_time(&self) -> u64 {
         let st = self.metadata().unwrap().modified().unwrap();
         st.duration_since(SystemTime::UNIX_EPOCH)
-            .expect("cannot get mtime")
+            .expect("cannot get mod_time")
             .as_secs()
     }
 
-    fn permissions(&self) -> Permissions {
+    pub fn permissions(&self) -> Permissions {
         self.metadata().unwrap().permissions()
     }
 
     fn overwrite_with(&self, with: &SynkFile) -> io::Result<u64> {
         fs::copy(with.p, self.p)
     }
+
+    fn decide(a: &'a SynkFile, b: &'a SynkFile) -> Option<Copier<'a>> {
+        let a_len = a.len();
+        let a_mod = a.mod_time();
+        let b_len = b.len();
+        let b_mod = b.mod_time();
+        if a_len == b_len {
+            None
+        } else if a_mod > b_mod && a_len > b_len {
+            Some(Copier { from: a, to: b })
+        } else if a_mod < b_mod && a_len < b_len {
+            Some(Copier { from: &b, to: &a })
+        } else {
+            None
+        }
+    }
 }
 
-fn sync(a: &SynkFile, b: &SynkFile) -> io::Result<Option<()>> {
-    let a_len = a.size();
-    let a_mod = a.mod_time();
-    let b_len = b.size();
-    let b_mod = b.mod_time();
-    let retval = if a_len == b_len {
-        None
-    } else if a_mod > b_mod && a_len > b_len {
-        b.overwrite_with(&a)?;
-        Some(())
-    } else if a_mod < b_mod && a_len < b_len {
-        a.overwrite_with(&b)?;
-        Some(())
-    } else {
-        None
+fn sync(a: &SynkFile, b: &SynkFile) -> io::Result<Option<u64>> {
+    let res = match SynkFile::decide(a, b) {
+        Some(c) => Some(c.to.overwrite_with(c.from)?),
+        _ => None,
     };
-    Ok(retval)
+    Ok(res)
 }
 
 fn main_loop(a: SynkFile, b: SynkFile) -> io::Result<()> {
     loop {
-        let res = sync(&a, &b);
-        match res {
-            Ok(Some(())) => println!("synced successfully"),
+        match sync(&a, &b) {
+            Ok(Some(_)) => println!("synced successfully"),
             Ok(None) => println!("no sync needed"),
             Err(err) => println!("{:?}", err),
         }
